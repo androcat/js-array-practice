ff47cfd17f1119805d9a67dca518f4b5cf29f4dd
Jay Medina <jay.medina.celis@gmail.com>
2023-01-09 14:47:06 -0500

    Complete third and fourth codewars

--- codewars.js
+++ codewars.js
@@ -26,9 +26,36 @@ const arr = function (N) {
 // Drone Fly-By
 // https://www.codewars.com/kata/58356a94f8358058f30004b5/train/javascript
 
+function flyBy(lamps, drone) {
+  let oes = "";
+  if (drone.length > lamps.length) {
+    for (let i = 0; i < lamps.length; i++) {
+      oes += "o";
+    }
+    lamps = oes;
+  } else {
+    for (let i = 0; i < drone.length; i++) {
+      oes += "o";
+    }
+    lamps = oes + lamps.slice(drone.indexOf("T") + 1);
+  }
+  return lamps;
+}
+
 // # 4
 // Get the mean of an array
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+
+function getAverage(marks) {
+  let sum = 0;
+  let avg;
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+  avg = sum / marks.length;
+  return Math.floor(avg);
+  //TODO : calculate the downward rounded average of the marks array
+}
 
 // #5
 // THE SOUL OF WIT: REVERSE AN ARRAY
