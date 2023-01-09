b0d69f02d2df0f58c8527c8ed234d162f690cfce
Jay Medina <jay.medina.celis@gmail.com>
2023-01-09 17:53:26 -0500

    Complete exercises

--- exercises.js
+++ exercises.js
@@ -1,18 +1,18 @@
 // ---------------------
-// Define a function that takes a string and an integer of i and creates a new array of length i where each value is equal to the string passed in
+// Define a function that takes a string and an integer of i and creates
+// a new array of length i where each value is equal to the string passed in
 // myFunction('sunshine', 3) => ['sunshine', 'sunshine', 'sunshine'];
 // ---------------------
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
-
+function multiArray(str, i) {
+  const arr = [];
+  for (let j = 0; j < i; j++) {
+    arr.push(str);
+  }
+  return arr;
+}
 
 // -----------------------------------------------
 
@@ -23,14 +23,13 @@
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
-
+function reverse(arr) {
+  let arrReversed = [];
+  for (let i = arr.length - 1; i > -1; i--) {
+    arrReversed.push(arr[i]);
+  }
+  return arrReversed;
+}
 
 // -----------------------------------------------
 
@@ -40,33 +39,30 @@
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
-
+function removeF(arr) {
+  const falsies = [undefined, null, NaN, 0, "", false];
+  return arr.filter(function (value) {
+    if (!falsies.includes(value)) return value;
+  });
+}
 
 // -----------------------------------------------
 
 // ---------------------
-// Define a function that takes an array of nested arrays and returns an object composed of propeties equal to each nested array
+// Define a function that takes an array of nested arrays and returns an object
+// composed of propeties equal to each nested array
 // const myArray = [['name', 'Charlie'], ['color', 'brown'], ['age', 10]];
 // returns { name: 'Charlie', color: 'brown', age: 10 };
 // ---------------------
 
 // Put your answer below -------------------------
-
-
-
-
-
-
-
-
-
+function arrayIntoObj(arr) {
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i][0]] = arr[i][1];
+  }
+  return obj;
+}
 
 // -----------------------------------------------
 
@@ -77,19 +73,16 @@
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
-
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
 
 // -----------------------------------------------
 
 // ---------------------
-// Define a function that takes two arrays and returns true if they have identical values (order does not matter), it should return false otherwise
+// Define a function that takes two arrays and returns true if
+// they have identical values (order does not matter), it should return
+// false otherwise
 // [1,2,3,4] and [1,2,3,4] should return true
 // [1,2,3,4,5] and [1,2,3,4] should return false
 // [1,2,3,4] and [1,2,3,4,4] should return false
@@ -98,14 +91,19 @@
 
 // Put your answer below -------------------------
 
+function sameValuesArray(arr1, arr2) {
+  let hasSameValue = false;
 
+  if (
+    arr1.every((i) => arr2.includes(i)) &&
+    arr2.every((j) => arr1.includes(j)) &&
+    arr1.length === arr2.length
+  ) {
+    hasSameValue = true;
+  }
 
-
-
-
-
-
-
+  return hasSameValue;
+}
 
 // -----------------------------------------------
 
@@ -128,15 +126,6 @@
 
 // Put your answer below -------------------------
 
-
-
-
-
-
-
-
-
-
 // -----------------------------------------------
 
 // ---------------------
@@ -146,14 +135,5 @@
 // ---------------------
 
 // Put your answer below -------------------------
-
-
-
-
-
-
-
-
-
 
 // -----------------------------------------------
